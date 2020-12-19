7f37e7fe9e6e44bc3d3d264fb057eef2e40b0572
burakcorekci <burakcorekcioglu@gmail.com>
2020-12-19 16:32:31 +0100

    cch 1.3 duplicate collection map

--- main/ch1/sub3.go
+++ main/ch1/sub3.go
@@ -1,7 +1,33 @@
 package ch1
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func Sub3() {
 	fmt.Print(" - Chapter 1.3\n\n")
+	duplicates()
+}
+
+func duplicates() {
+	fmt.Println("Duplicates Map")
+
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		txt := input.Text()
+		if txt == "break" {
+			break
+		}
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+	fmt.Println()
 }
